e4f898bd696e13890ba5e39471ef0702308f3922
MOJOEA <DevineTYpor@gmail.com>
2026-08-19 16:28:28 +0700

    new api

--- LSTRIDE-REST-API/src/app.ts
+++ LSTRIDE-REST-API/src/app.ts
@@ -7,10 +7,14 @@ import { authMiddleware } from "./middleware/auth.middleware";
 
 import authRoutes from "./routes/auth.routes";
 import userRoutes from "./routes/user.routes";
-//import postRoutes from "./routes/post.routes";
+import followRoutes from "./routes/follow.routes";
+
+import postRoutes from "./routes/post.routes";
+import likeRoutes from "./routes/like.routes";
+import commentRoutes from "./routes/comment.routes";
+
 
 const app = express();
-
 
 app.use(express.json());
 
@@ -19,7 +23,13 @@ app.use("/api/auth", authRoutes);
 app.get("/health", (req, res) => { res.json({ success: true, message: "API is running",});});
 
 app.use("/api/users",authMiddleware, userRoutes);
-// app.use("/api/users", authMiddleware, userRoutes);
-// app.use("/api/posts", authMiddleware, postRoutes);
+
+app.use("/api/follow",authMiddleware, followRoutes);
+
+app.use("/api/posts",authMiddleware, postRoutes);
+
+app.use("/api/likes",authMiddleware, likeRoutes);
+
+app.use("/api/comments",authMiddleware, commentRoutes);
 
 export default app;
